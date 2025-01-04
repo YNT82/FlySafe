a173d6d900cb2d85a98a5aef226bad6b107d1fdf
user <user@WINDOWS-FGBEH01>
2025-01-04 04:42:21 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace FlySafe
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Устанавливаем начальную позицию окна в центре экрана
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        // Обработчик для перетаскивания окна
+        private void Grid_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                this.DragMove();  // Перетаскивание окна
+            }
+        }
+
+        // Обработчик правой кнопки мыши на Label ECAM MESSAGE TEST
+        private void ECAMLabel_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // Создаем и показываем новое окно
+            NewWindow newWindow = new NewWindow();
+
+            // Устанавливаем владельца окна, чтобы оно отображалось на переднем плане
+            newWindow.Owner = this;
+
+            // Получаем позицию лейбла относительно главного окна
+            Point labelPosition = ECAMLabel.TransformToAncestor(this).Transform(new Point(0, 0));
+
+            // Устанавливаем окно точно на позиции лейбла
+            newWindow.Left = this.Left + labelPosition.X;
+            newWindow.Top = this.Top + labelPosition.Y;
+
+            // Показываем окно как модальное
+            newWindow.ShowDialog();
+        }
+    }
+}
---
+++ Options.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace FlySafe
+{
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик для левого клика мыши по окну (для закрытия окна)
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // Закрытие окна при клике
+            this.Close();
+        }
+
+        // Обработчик для события "Чекбокс отмечен"
+        private void AlwaysOnTopCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // Галочка становится видимой при установке чекбокса в состояние "отмечено"
+            var checkMark = (Path)((CheckBox)sender).Template.FindName("CheckMark", (FrameworkElement)sender);
+            checkMark.Opacity = 1;  // Показываем галочку
+        }
+
+        // Обработчик для события "Чекбокс снят"
+        private void AlwaysOnTopCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // Галочка исчезает при снятии чекбокса
+            var checkMark = (Path)((CheckBox)sender).Template.FindName("CheckMark", (FrameworkElement)sender);
+            checkMark.Opacity = 0;  // Скрываем галочку
+        }
+    }
+}
