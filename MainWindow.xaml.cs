ee08a0e56f95a7d1dd0ab89988900006108327c3
YNT <Ulrich.jt@gmail.com>
2025-01-05 22:21:40 +0300

    MessageProcessor moved to separate class.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -202,11 +202,11 @@ namespace FlySafe
         {
             // Строки для поиска
             List<string> searchStrings = new List<string>
-            {
-                "Wind shear",
-                "Landing lights",
-                "Spoilers not armed",
-                "Simbrief type does not match flight type"
+             {
+             "Wind shear",
+             "Landing lights",
+             "Spoilers not armed",
+             "Simbrief type does not match flight type"
             };
 
             // Списки для результатов
@@ -214,8 +214,8 @@ namespace FlySafe
             List<string> checkTypes;
             List<string> sections;
 
-            // Вызов функции поиска
-            MessageSearcher.SearchMessages(searchStrings, out messages, out checkTypes, out sections);
+            // Вызов функции обработки сообщений и изменения цветов
+            MessageProcessor.ProcessMessages(searchStrings, out messages, out checkTypes, out sections);
 
             // Путь к файлу для сохранения результатов
             string resultFilePath = "result.txt";
@@ -241,72 +241,6 @@ namespace FlySafe
                     writer.WriteLine(section);
                 }
             }
-            // Проверка наличия разных секций и изменение цвета кнопки в зависимости от секции
-            switch (sections.Contains("Warnings"))
-            {
-                case true:
-                    ButtonStyleHelper.SetWarningButtonColor("#FF0000");
-                    break;
-                case false:
-                    ButtonStyleHelper.SetWarningButtonColor("#800000");
-                    break;
-            }
-
-            switch (sections.Contains("Cautions"))
-            {
-                case true:
-                    ButtonStyleHelper.SetCautionButtonColor("#FF8C00");
-                    break;
-                case false:
-                    ButtonStyleHelper.SetCautionButtonColor("#806000");
-                    break;
-            }
-
-            // Проверка наличия разных типов и изменение цвета лейблов в зависимости от типа
-            switch (checkTypes.Contains("Flight plan"))
-            {
-                case true:
-                    LabelStyleHelper.SetFuelLabelColor("#FF8C00");
-                    break;
-                case false:
-                    LabelStyleHelper.SetFuelLabelColor("#806000");
-                    break;
-            }
-
-            switch (checkTypes.Contains("Systems"))
-            {
-                case true:
-                    LabelStyleHelper.SetApuLabelColor("#FF8C00");
-                    break;
-                case false:
-                    LabelStyleHelper.SetApuLabelColor("#806000");
-                    break;
-            }
-
-            switch (checkTypes.Contains("Flight control"))
-            {
-                case true:
-                    LabelStyleHelper.SetFlightControlLabelColor("#FF8C00");
-                    break;
-                case false:
-                    LabelStyleHelper.SetFlightControlLabelColor("#806000");
-                    break;
-            }
-
-            switch (checkTypes.Contains("Weather"))
-            {
-                case true:
-                    LabelStyleHelper.SetAntiIceLabelColor("#FF8C00");
-                    break;
-                case false:
-                    LabelStyleHelper.SetAntiIceLabelColor("#806000");
-                    break;
-            }
-
-
-
-
-
             // Информация об успешном завершении
             MessageBox.Show("Результаты сохранены в файл result.txt");
         }
